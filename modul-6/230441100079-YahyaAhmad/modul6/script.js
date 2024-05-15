ce4fdc19d406ddd6341006d92e7e8ccbfb085c0d
Yahya Ahmad <106219715+yhyaahmd@users.noreply.github.com>
2024-05-15 22:31:45 +0700

    230441100079_YahyaAhmad_Modul6_AsprakKakArdi

---
+++ modul-6/230441100079-YahyaAhmad/modul6/script.js
@@ -0,0 +1,4 @@
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signUp');
